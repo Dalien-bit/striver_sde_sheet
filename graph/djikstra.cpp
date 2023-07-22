e7f237d18fb1d8ca3f0f43a847fe3182319902a0
Dalien-bit <dalien19861@gmail.com>
2023-07-22 12:01:59 +0530

    added shortest path from 1 to n

--- graph/djikstra.cpp
+++ graph/djikstra.cpp
@@ -30,4 +30,49 @@ public:
   }
 };
 
+typedef vector<vector<pair<int, int>>> vvpii;
+class Solution {
+public:
+  vector<int> shortestPath(int n, int m, vector<vector<int>> &edges) {
+    vvpii adj(n + 1);
+    for (int i = 1; i <= n; i++) {
+      adj[edges[i][0]].push_back({adj[i][1], adj[i][2]});
+      adj[edges[i][1]].push_back({adj[i][0], adj[i][2]});
+    }
+    int vis[n + 1] = {0};
+    
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    
+    vector<int> dist(V, INF);
+    pq.push({0, S});
+    dist[S] = 0;
+
+    vector<int> parent(n + 1);
+    for (int i = 1; i <= n; i++) parent[i] = i;
+
+    while (!pq.empty()) {
+      int distNode = pq.top().first, node = pq.top().second;
+      pq.pop();
+
+      for (int i = 0; i < adj[node].size(); i++) {
+        int child = adj[node][i].first, wt = adj[node][i].second;
+        if (distNode + wt < dist[child]) {
+          dist[child] = distNode + wt;
+          parent[child] = node;
+          pq.push({distNode + wt, child});
+        }
+      }
+    }
+
+    vector<int> ans;
+    int node = n;
+    while (node != parent[node]) {
+      ans.push_back(node);
+      node = parent[node];
+    }
+
+    reverse(ans.begin(), ans.end());
+  }
+};
+
 int main() { return 0; }
